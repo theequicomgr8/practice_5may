a6459c2250216b23da5280715bea3199cbf122db
Suman <suman@gmail.com>
2022-05-04 21:03:00 +0530

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
